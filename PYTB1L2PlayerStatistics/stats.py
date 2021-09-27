66b03297df1dd0881f5c9f902599dc99365afcb3
Nabil <33580@ma-web.nl>
2021-09-27 13:10:02 +0200

    player stats

---
+++ PYTB1L2PlayerStatistics/stats.py
@@ -0,0 +1,20 @@
+naam = "player1"
+print("name: " + naam)
+ammo = 15 
+print("ammo: " + str(ammo))
+speed = 8 
+print("speed: " + str(speed))
+ability = "Hover"
+print("ability: " + ability )
+lives = 5
+print("Lives: " + str(lives))
+wanted = True
+print("wanted: " + str(wanted))
+level = 6
+print("level: " + str(level))
+exp = "12/34"
+print("exp: " + exp)
+percentage = "56%"
+print("charge: " + percentage)
+fuel = "87%" 
+print("fuel: " + fuel)
