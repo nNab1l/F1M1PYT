93b89ee66cb928014d67239596519bc6ab951174
Nabil <33580@ma-web.nl>
2021-10-28 12:06:50 +0200

    shoppinglist

---
+++ PYTB1L6Groceries/shoppinglist.py
@@ -0,0 +1,16 @@
+
+grocerylist = ["milk", "bread", "cake", "water",]
+
+Shoppinglist = ["milk", "bread", "cake", "water",]
+
+
+if grocerylist == Shoppinglist:
+    print("Done shopping")
+
+else: print("continue shopping")
+
+
+
+
+
+
